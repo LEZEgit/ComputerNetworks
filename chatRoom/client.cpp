8b5bdf6707e9be6317a027f8effc221e43593bbf
LEZEgit <rjekm6940@gmail.com>
2026-02-13 01:02:58 +0530

    Set up multithreading at client and server side

--- chatRoom/client.cpp
+++ chatRoom/client.cpp
@@ -9,6 +9,27 @@
 using namespace std;
 #define PORT 8080
 
+void exit_the_chat(const int fd){
+  // Close socket
+  cout<<"you have successfully left the chat\n";
+  close(fd);
+}
+
+// Receive thread
+void* handle_recv(void* sock_fd) {
+    int fd = *(int*)sock_fd;
+    char buffer[1024];
+    while (true) {
+        int bytes = recv(fd, buffer, 1024, 0);
+        if (bytes <= 0) break;
+        buffer[bytes] = '\0';
+        cout << buffer << endl;
+    }
+    exit_the_chat(fd);
+    return nullptr;
+}
+
+
 int main() {
   char buffer[1024];
 
@@ -43,14 +64,48 @@ int main() {
     cout<<"Server says: "<<buffer<<endl;
   }
 
-  // Send data
+  // User registration
   cout<<"Enter your username: ";
   string msg;
   getline(cin, msg);
   send(sock, msg.data(), msg.size(),0);
-  cout << "Message sent" << endl;
+  int bytes = recv(sock, buffer, sizeof(buffer) - 1, 0);
+  if(bytes <= 0){
+    cout<<"Lost connection to server\n";
+    // break;
+  }
+  else{
+    cout<<buffer<<endl;
+  }
 
-  // Close socket
-  close(sock);
+    /*
+      will use another thread for receiving messages and main thread for sending the message
+      if main thread for receiving
+      and p_thread for sending
+      terminating is complex (send EXIT to server and again receive EXIT from server and then terminate the main thread)
+
+      therefore we do the sending from main thread,
+      if user enters EXIT, main thread terminates (client disconnects)
+    */
+  cout<<"\n=====You can now send messages=====\n";
+    
+  string message;
+    pthread_t recv_thread_id;
+    pthread_create(&recv_thread_id, NULL, handle_recv, (void*)&sock);
+    pthread_detach(recv_thread_id); // Let thread clean up itself
+
+    // sending loop
+    bool exit = false;
+    while(true){
+        getline(cin, message);
+        if(message == "@EXIT"){
+            exit = true;
+        }
+        if(message == "@LIST" || message == "@EXIT")
+            message += " ";
+        send(sock, message.data(), message.size(), 0);
+        if (exit) break;
+    }
+    exit_the_chat(sock);
   return 0;
 }
--- chatRoom/server.cpp
+++ chatRoom/server.cpp
@@ -5,37 +5,139 @@
 #include <cstring>
 #include <iostream>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 #define PORT 8080
 
-struct thread_data {
-  string cmd;
-  string message;
-};
+
 
 unordered_map<string, int> username_to_clientfd;
+unordered_map<int, string> clientfd_to_username;
 
-void *handle_client(void *threadarg) {
-  struct thread_data *my_data;
-  my_data = (struct thread_data *) threadarg;
-  
-  string cmd = my_data->cmd;
-  string message = my_data->message;
+unordered_set<int> active_client_fds;
 
-  if(cmd == "@all" || cmd == ""){
-    // send msg to all clients
-    cout<</*client_name*/": "<<message<<endl;
-  } else if(cmd[0] == '@'){
-    string recv_client_username = cmd.substr(1);
-    cout<<"Send message to "<<recv_client_username<<endl;
-  } else if(cmd == "LIST"){
-    // list all users
-  } else if(cmd == "EXIT"){
-    cout<<"User "<</*client_name*/" has left the chat\n";
+void send_to_all_clients(const string response, const int sender_fd){
+  for(int fd : active_client_fds){
+    if(fd == sender_fd) continue;  
+    send(fd, response.data(), response.size(), 0);
   }
-   
+}
+
+
+void delete_client(const string client_username){
+  int client_fd = username_to_clientfd[client_username];
+  active_client_fds.erase(client_fd);
+  username_to_clientfd.erase(client_username);
+  clientfd_to_username.erase(client_fd);
+  /*
+    should i close the thread here or in the handle_client??
+    ok so first we close the client_socket
+    then exit the thread
+  */
+  string res = client_username + " has left the chat\n";
+  cout<<res;
+  send_to_all_clients(res, -1);
+  // Close client socket
+  close(client_fd);
+}
+
+void send_active_clients_list(int client_fd){
+  string response = "";
+  for(auto e : username_to_clientfd){
+    response += e.first;
+    response += string(1, '\n');
+  }
+  send(client_fd, response.data(), response.size(), 0);
+}
+
+// struct thread_data {
+//   string cmd;
+//   string message;
+//   int client_fd;
+//   string sender_name;
+// };
+void *handle_client(void *arg) {
+  int client_fd = *(int*) arg;
+  string sender_name = clientfd_to_username[client_fd];
+  char buffer[1024];
+
+  while(true){
+    int bytes = recv(client_fd, buffer, sizeof(buffer)-1, 0);
+    if (bytes < 0) break;
+
+    if (bytes == 0) continue; // if sent empty message ignore it
+
+    buffer[bytes] = '\0';
+    string msg(buffer);
+    string cmd = "";
+    string message = "";
+    string response = "";
+    if(msg[0] == '@'){
+      int delim_pos = msg.find(' '); // will make sure at the client side to append a space at the end of @LIST or @EXIT command
+      if(delim_pos != string::npos){
+        cmd = msg.substr(0, delim_pos); 
+        if(delim_pos+1 < msg.size()) message = msg.substr(delim_pos+1);
+      }
+    } else{
+      message = msg;
+    }
+    // if(message.size()==0){
+    //   response = "Can't send empty message\n";
+    //   send(client_fd, response.data(), response.size(), 0);
+    //   continue;
+    // }  
+    response = sender_name + ": " + message;
+    if(cmd == "@LIST"){
+      send_active_clients_list(client_fd);
+    } else if(cmd == "@EXIT"){
+      break;
+    } else if(cmd == "@all" || cmd == ""){
+      send_to_all_clients(response, client_fd);
+      cout<< sender_name << " (to @all)\n\t"<<message<<endl;
+    } else if(cmd[0] == '@'){
+      string recv_client_username = cmd.substr(1);
+      if(username_to_clientfd.find(recv_client_username) == username_to_clientfd.end()){
+        string nf_msg = recv_client_username + " is not in the chat\n"; 
+        cout<<nf_msg<<endl;
+        send(client_fd, nf_msg.data(), nf_msg.size(), 0);
+      }
+      int recv_client_fd = username_to_clientfd[recv_client_username];
+      send(recv_client_fd, response.data(), response.size(), 0);
+      cout<<sender_name<<" sent a private message to "<<recv_client_username<<endl;
+    } 
+  }
+  delete_client(sender_name);
   pthread_exit(NULL);
 }
+
+void register_client(const int client_fd){
+  // Read client's username
+  char buffer[1024] = {0};
+  read(client_fd, buffer, 1024);
+  // add client to map
+  username_to_clientfd[buffer] = client_fd;
+  clientfd_to_username[client_fd] = buffer;
+  // add client to active users
+  if(active_client_fds.count(client_fd) == 0)
+    active_client_fds.insert(client_fd);
+  else {
+    cout<<"Couldn't add client_fd to active clients\n";
+    return;
+  }
+  string res = buffer; 
+  res += " joined the chat\n";
+  send_to_all_clients(res, client_fd);
+  cout<< res;
+
+  // assign thread to each client
+  pthread_t thread_id;
+  pthread_create(&thread_id, NULL, handle_client, (void*)&client_fd);
+  pthread_detach(thread_id); // Let thread clean up itself
+}
+
+
+
+
 
 int main() {
   int server_fd, client_fd;
@@ -64,33 +166,23 @@ int main() {
   }
 
   // Listen
-  if (listen(server_fd, 5) < 0) {
-    perror("listen");
-    exit(EXIT_FAILURE);
-  }
+  cout<<"Listening...\n\n";
+  while(listen(server_fd, 5)>=0){
+    // Accept a connection
+    client_fd =
+        accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+    if (client_fd < 0) {
+      perror("accept");
+      exit(EXIT_FAILURE);
+    } else{
+      string response = "Connection established\n";
+      send(client_fd, response.data(), response.size(), 0);
+    }
+        
+    register_client(client_fd);
+  } 
 
-  // Accept a connection
-  client_fd =
-      accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-  if (client_fd < 0) {
-    perror("accept");
-    exit(EXIT_FAILURE);
-  } else{
-    string response = "Connection established\n";
-    send(client_fd, response.data(), response.size(), 0);
-  }
-
-
-
-  // Read client's username
-  read(client_fd, buffer, 1024);
-  // add client to map
-  username_to_clientfd[buffer] = client_fd;
-  cout<<"client_fd: "<<client_fd<<endl;
-  cout<< buffer << " joined the chat" << endl;
-
-  // Close socket
-  close(client_fd);
+  // Close server socket
   close(server_fd);
   return 0;
 }
